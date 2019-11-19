15f71262613a978471db1928299caced4f1a20f0
Amit Kumar <axk507@AUMLBRO-WS1099>
2019-11-20 08:42:41 +1100

    removed components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,26 +7,11 @@ import { AppComponent } from './app.component';
 
 import { FormComponent } from './sample/form/form.component';
 import { BsControlsModule } from 'projects/bs-controls/src/public-api';
-import { FormTextComponent } from './sample/form-text/form-text.component';
-import { FormFormatComponent } from './sample/form-format/form-format.component';
-import { FormListsComponent } from './sample/form-lists/form-lists.component';
-import { FormDatesComponent } from './sample/form-dates/form-dates.component';
-import { FormChecksComponent } from './sample/form-checks/form-checks.component';
-import { FormRegexComponent } from './sample/form-regex/form-regex.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    FormComponent,
-    FormTextComponent,
-    FormFormatComponent,
-    FormListsComponent,
-    FormDatesComponent,
-    FormChecksComponent,
-    FormRegexComponent
-  ],
+  declarations: [AppComponent, FormComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
--- src/app/sample/form/form.component.ts
+++ src/app/sample/form/form.component.ts
@@ -6,10 +6,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form.component.css']
 })
 export class FormComponent implements OnInit {
+  email1: string;
+  email2: string;
+  url1: string;
+  url2: string;
+  text1: string;
+  text2: string;
+  text3: string;
+  text4: string;
+  pwd1: string;
+  pwd2: string;
 
-  constructor() { }
+  number1: string;
+  number2: string;
+  number3: string;
+
+  date2str: string;
+  check1: boolean;
+  constructor() {}
 
   ngOnInit() {
+    this.initData();
   }
 
+  initData() {
+    this.email1 = 'ghib3003@bigpond.com';
+    this.email2 = undefined;
+    this.url1 = 'https://resme.ddns.net/';
+    this.url2 = undefined;
+    this.text1 = 'Required text box';
+    this.text2 = undefined;
+    this.text3 = undefined;
+    this.text4 = 'Not mandatory text area';
+    this.pwd1 = undefined;
+    this.pwd2 = undefined;
+    this.number1 = '1234';
+    this.number2 = undefined;
+    this.number3 = undefined;
+    this.date2str = '1979-07-24T14:00:00.000';
+  }
 }
